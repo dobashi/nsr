fca70e0971c6212295373b8531f2ef5a2497a1e4
ten <ten@lavans.jp>
2017-08-05 04:13:56 +0800

    name refactor

--- src/main/scala/com/lavans/nsr/Main.scala
+++ src/main/scala/com/lavans/nsr/Main.scala
@@ -2,28 +2,23 @@ package com.lavans.nsr
 
 import java.io.PrintWriter
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties
-import com.fasterxml.jackson.databind.ObjectMapper
-import com.fasterxml.jackson.module.scala.DefaultScalaModule
-
 import scala.io.Source
 
 /**
   * Created by ten on 17/07/31.
   */
 object Main {
-  val om = new ObjectMapper()
-  om.registerModule(DefaultScalaModule)
-  val file = "src/main/resources/datafile.csv"
-  val outputFile = "src/main/resources/result.csv"
+
+  val inputfile = "src/main/resources/input.csv"
+  val outputFile = "src/main/resources/output.csv"
 
   def main(args: Array[String]): Unit = {
-    val log = org.log4s.getLogger
-    using(Source.fromFile(file)) { in =>
+    using(Source.fromFile(inputfile)) { in =>
       using(new PrintWriter(outputFile)) { out =>
         in.mkString.split("\n")
-          .map(ApiRequester.get)
-          .flatMap(parse(_).map(_.toDataString))
+          .map(ApiRequest.get)
+          .flatMap(ApiResponse.parse(_).firstItem)
+          .map(_.toDataString)
           .map(out.println)
       }
     }
@@ -32,29 +27,6 @@ object Main {
   def using[A <% {def close() : Unit}](s: A)(f: A => Any) {
     try f(s) finally s.close()
   }
-
-  def parse(json: String): Option[Item] ={
-    val resp = om.readValue(json, classOf[ApiResponse])
-    if(resp.count>0) resp.Items(0).get("Item") else None
-  }
 }
 
-case class ApiResponse(
-  count: Int, page: Int, first: Int, last: Int, hits: Int, carrier: Int, pageCount: Int, Items: List[Map[String,Item]], GenreInformation:List[Any]
-)
-
-@JsonIgnoreProperties(ignoreUnknown=true)
-case class Item(
-  title: String,
-  titleKana: String,
-  artistName: String,
-  artistNameKana: String,
-  label: String,
-  jan: String,
-  makerCode: String
-){
-  def toDataString = {
-    s"$jan, $makerCode, $artistName, $title"
-  }
-}
 
